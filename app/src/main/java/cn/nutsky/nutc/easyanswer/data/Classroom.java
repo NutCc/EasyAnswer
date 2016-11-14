d3b84fc8cdb7c007c67e1bb19fffc82cd9751976
NutCc <951284984@qq.com>
2016-11-15 00:35:07 +0800

    龟速写界面中……

---
+++ app/src/main/java/cn/nutsky/nutc/easyanswer/data/Classroom.java
@@ -0,0 +1,19 @@
+package cn.nutsky.nutc.easyanswer.data;
+
+/**
+ * Created by NutC on 2016/11/15.
+ */
+
+public class Classroom {
+    private String classContent; //教室内容
+    private String content;      //答疑内容
+    private String name;         //user昵称
+    private String endTime;      //答疑结束时间
+    private String beginTime;    //答疑开始时间
+    private String createdAt;    //创建时间
+    private String objectId;     //答疑id
+
+
+
+    
+}
